c0e1d5910fcac8daa3567ac0f12fbe9ec957e5ce
dadabhau <dadabhaut@ithena.ai>
2021-06-28 13:50:57 +0530

    Transforming Response Data

--- src/app/shared/data.storage.service.ts
+++ src/app/shared/data.storage.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs/operators';
+import { pipe } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class DataStoragesService {
@@ -23,8 +25,20 @@ export class DataStoragesService {
       });
   }
   fetchRecipes() {
-    this.http.get<Recipe[]>(this.recipesUrl).subscribe((recipes) => {
-      this.recipesService.setRecipe(recipes);
-    });
+    this.http
+      .get<Recipe[]>(this.recipesUrl)
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
+      .subscribe((recipes) => {
+        this.recipesService.setRecipe(recipes);
+      });
   }
 }
