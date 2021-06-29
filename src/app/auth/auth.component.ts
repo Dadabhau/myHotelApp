9f376718e1d6cd29f3d0626de5f812235a70a26a
dadabhau <dadabhaut@ithena.ai>
2021-06-29 22:06:37 +0530

    Sending the Signup Request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -7,11 +8,35 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthComponent {
   isLoginmode = true;
+
+  constructor(private authService: AuthService) {}
+
   onSwitchMode() {
     this.isLoginmode = !this.isLoginmode;
   }
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    //console.log(form.value);
+
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginmode) {
+      //...
+    } else {
+      this.authService.signup(email, password).subscribe(
+        (resData) => {
+          console.log(resData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+
     form.reset();
   }
 }
